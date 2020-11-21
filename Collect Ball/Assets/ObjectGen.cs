5d03b57de2139b2c13db53c20d3daff5926b0fd4
Samraj Singh Solanki <samrajsinghsolanki@gmail.com>
2020-11-21 19:37:52 +0530

    first commit
    added game folder to github

---
+++ Collect Ball/Assets/ObjectGen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGen : MonoBehaviour
+{
+	public  GameObject genThisObject;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        int t=Random.Range(5,15);
+		Invoke("EndThis",60);
+		Invoke("genObject",t);
+    }
+	
+	void genObject(){
+	 Instantiate(genThisObject,transform.position,Quaternion.identity);
+	 int t=Random.Range(5,15);
+	 Invoke("genObject",t);
+	 
+	}
+	void EndThis(){
+	Time.timeScale=0;
+	}
+}
---
+++ Collect Ball/Assets/moveBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class moveBar : MonoBehaviour
+{
+	public float speed;
+	public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+       score.text="0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+             //("Hello"); 
+       // Debug.Log(Input.GetAxis("Horizontal"));			 
+       float hori=Input.GetAxis("Horizontal");
+	    float ver=Input.GetAxis("Vertical");
+	   Vector3 movement=new Vector3(hori,ver);
+        transform.position+= speed*movement*Time.deltaTime;	   
+	}
+	private void OnCollisionEnter(Collision other){
+		int k=int.Parse(score.text)+1;
+		score.text=k.ToString();
+		}
+		
+}
